d31d9e780be65bb6b4c96e16fab45c26acabc41a
kiran199326 <kirankumar.dot.dot@gmail.com>
2020-07-26 06:58:33 +0530

    added new script
    Co-Authored-By: null <kiran199326@users.noreply.github.com>

---
+++ iterable.py
@@ -0,0 +1,10 @@
+# iterable example
+my_dic={
+ 'me':1,
+ 'you':2,
+ 'us':3
+}
+for item in my_dic.items():print(item)#prints key value tuples
+for item in my_dic.values():print(item)#prints key value
+for item in my_dic.keys():print(item)#prints keys
+for item in my_dic:print(item)#prints keys
