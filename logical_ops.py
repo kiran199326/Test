ffa9f8be2619c410af1400db3f41660f65d3a642
kiran199326 <kirankumar.dot.dot@gmail.com>
2020-07-26 06:31:54 +0530

    Added scripts
    Co-Authored-By: null <kiran199326@users.noreply.github.com>

---
+++ logical_ops.py
@@ -0,0 +1,6 @@
+#Logical operator
+is_magician= True
+is_expert= False
+if is_magician and is_expert: print("You are a master Magician!!")
+elif not is_magician and is_expert: print("You need Magic powers!!")
+elif is_magician and not is_expert: print("You are getting there!!")
---
+++ ternary_operator.py
@@ -0,0 +1,5 @@
+#Terenary operator
+# <syntax> condition_if_true if condition else condition
+is_friend = False
+can_msg = "Can Messsage" if is_friend else "Cannot Message"
+print(can_msg)
