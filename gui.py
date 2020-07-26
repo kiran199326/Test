439f8d29570be6c8c7adfbde5fdd75289f4ac77b
kiran199326 <kirankumar.dot.dot@gmail.com>
2020-07-27 02:51:52 +0530

    modified

--- gui.py
+++ gui.py
@@ -5,10 +5,9 @@ my_list=[
      [1,1,1,1,1,1,1],
      [0,0,0,1,0,0,0],
      [0,0,0,1,0,0,0]
-
 ]
-for i in my_list:
-    for j in i:
-        if j == 1:print("#",end=" ")
+for row in my_list:
+    for pixel in row:
+        if pixel == 1:print("#",end=" ")
         else:print(" ",end=" ")
     print(" ")
